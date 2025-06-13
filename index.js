c64d7da143b8c4bc59b57d03b1ec62dce80a7cf3
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-06-13 20:34:39 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainContent = document.querySelector("main#main");
+document.querySelector("body").remove(mainContent);
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "Nayara is the champion"
